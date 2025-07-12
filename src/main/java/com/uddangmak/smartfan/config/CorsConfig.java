2f52425a840ee3c7bca76a56bf8815f4243a15f5
dohyunk58 <hedge3x@gmail.com>
2025-07-12 21:14:13 +0900

    fix: Add CorsOrigin in CorsConfig.java

--- src/main/java/com/uddangmak/smartfan/config/CorsConfig.java
+++ src/main/java/com/uddangmak/smartfan/config/CorsConfig.java
@@ -1,6 +1,5 @@
 package com.uddangmak.smartfan.config;
 
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,11 +9,12 @@ public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**") // CORS 적용 API 경로
-                .allowedOrigins("http://localhost:5173", "http://127.0.0.1:5173") // React 개발 서버 주소
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용 HTTP 메서드
-                .allowedHeaders("*") // 허용 요청 헤더
-                .allowCredentials(true) // 자격 증명(쿠키, HTTP 인증) 허용
-                .maxAge(3600); // Pre-flight 요청 결과 캐시 시간
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173", "https://uddangmak.vercel.app")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("Authorization", "Content-Type")
+                .exposedHeaders("Custom-Header")
+                .allowCredentials(true)
+                .maxAge(3600);
     }
 }
